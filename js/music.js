bd29e1da6aa9cbd8b9e92b0fa141651d6ede47da
yiloko <168342875+yilogit@users.noreply.github.com>
2026-03-23 18:36:52 +0800

    Add files via upload

---
+++ js/music.js
@@ -0,0 +1,156 @@
+// 加载歌曲
+window.addEventListener("DOMContentLoaded",function(){
+const ap = new APlayer({
+    container: document.getElementById("music"),
+    lrcType: 3,
+    fixed: true,
+    audio: [
+            {
+                name: "HELP!!",
+                artist: "Kobo Kanaeru",
+                url: "./music/HELP!!.mp3", 
+                cover: "./images/music/HELP!!.jpg",
+                lrc: "./lrc/HELP!!.lrc"
+            },
+            {
+                name: "ワレワレは宇宙人だ。",
+                artist: "Sooda",
+                url: "./music/ワレワレは宇宙人だ。.mp3",
+                cover: "./images/music/ワレワレは宇宙人だ。.jpg",
+                lrc: "./lrc/ワレワレは宇宙人だ。.lrc"
+            },
+            {
+                name: "ソラゴト",
+                artist: "明透",
+                url: "./music/ソラゴト.mp3",
+                cover: "./images/music/ソラゴト.jpg",
+                lrc: "./lrc/ソラゴト.lrc"
+            },
+            {
+                name: "心拍数#0822",
+                artist: "鹿乃",
+                url: "./music/心拍数0822.mp3",
+                cover: "./images/music/心拍数0822.jpg",
+                lrc: "./lrc/心拍数0822.lrc"
+            },
+            {
+                name: "Re: searchlight",
+                artist: "Aiobahn/やなぎなぎ",
+                url: "./music/Re searchlight.mp3",
+                cover: "./images/music/Re searchlight.jpg",
+                lrc: "./lrc/Re searchlight.lrc"
+            },
+            {
+                name: "オノマトペ",
+                artist: "羽生まゐご/りりあ。",
+                url: "./music/オノマトペ.mp3",
+                cover: "./images/music/オノマトペ.jpg",
+                lrc: "./lrc/オノマトペ.lrc"
+            },
+            {
+                name: "Steppin' Up Life!",
+                artist: "鬼頭明里",
+                url: "./music/Steppin' Up Life!.mp3",
+                cover: "./images/music/Steppin Up Life!.jpg",
+                lrc: "./lrc/Steppin' Up Life!.lrc"
+            },
+            {
+                name: "ライアーメイデン (feat. りぃふ)",
+                artist: "ヤバス/りぃふ",
+                url: "./music/ライアーメイデン (feat. りぃふ).mp3",
+                cover: "./images/music/ライアーメイデン (feat. りぃふ).jpg",
+                lrc: "./lrc/ライアーメイデン (feat. りぃふ).lrc"
+            },
+            {
+                name: "everything",
+                artist: "Studio 'Syrup Comfiture'",
+                url: "./music/everything.mp3",
+                cover: "./images/music/everything.jpg",
+                lrc: "./lrc/everything.lrc"
+            },
+            {
+                name: "3253212",
+                artist: "reche",
+                url: "./music/3253212.mp3",
+                cover: "./images/music/3253212.jpg",
+                lrc: "./lrc/3253212.lrc"
+            },
+            {
+                name: "妄想感傷代償連盟",
+                artist: "YuNi",
+                url: "./music/妄想感傷代償連盟.mp3",
+                cover: "./images/music/妄想感傷代償連盟.jpg",
+                lrc: "./lrc/妄想感傷代償連盟.lrc"
+            },
+            {
+                name: "Life We Sow",
+                artist: "Mili",
+                url: "./music/Life We Sow.mp3",
+                cover: "./images/music/Life We Sow.jpg",
+                lrc: "./lrc/Life We Sow.lrc"
+            },
+            
+            {
+                name: "In My Room (feat. KMNZ LIZ)",
+                artist: "キツネDJ",
+                url: "./music/In My Room (feat. KMNZ LIZ).mp3",
+                cover: "./images/music/In My Room (feat. KMNZ LIZ).jpg",
+                lrc: "./lrc/In My Room (feat. KMNZ LIZ).lrc"
+            },
+            {
+                name: "砂のこども (feat. 水瀬ましろ)",
+                artist: "水瀬ましろ",
+                url: "./music/砂のこども (feat. 水瀬ましろ).mp3",
+                cover: "./images/music/砂のこども (feat. 水瀬ましろ).jpg",
+                lrc: "./lrc/砂のこども (feat. 水瀬ましろ).lrc"
+            },
+            {
+                name: "ネロイズム",
+                artist: "かいりきベア/鳴花ミコト",
+                url: "./music/ネロイズム.mp3",
+                cover: "./images/music/ネロイズム.jpg",
+                lrc: "./lrc/ネロイズム.lrc"
+            },
+            {
+                name: "神様のいうとおり (Version Z80)",
+                artist: "やくしまるえつこ",
+                url: "./music/神様のいうとおり (Version Z80).mp3",
+                cover: "./images/music/神様のいうとおり (Version Z80).jpg",
+                lrc: "./lrc/神様のいうとおり (Version Z80).lrc"
+            },
+            {
+                name: "You",
+                artist: "雪野五月",
+                url: "./music/You.mp3",
+                cover: "./images/music/You.jpg",
+                lrc: "./lrc/You.lrc"
+            },
+            {
+                name: "かみさまちゃん",
+                artist: "ど〜ぱみん/歌愛ユキ",
+                url: "./music/かみさまちゃん.mp3",
+                cover: "./images/music/かみさまちゃん.jpg",
+                lrc: "./lrc/かみさまちゃん.lrc"
+            },
+ ]});})
+//  加载完毕
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
